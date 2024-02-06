72065a88316e4d7223e65921463e3b397d7e726b
Anand Sharma <84133329+anand777sharma@users.noreply.github.com>
2024-02-06 16:22:28 +0530

    ..

--- backend/controllers/file_controller.js
+++ backend/controllers/file_controller.js
@@ -1,9 +1,9 @@
 
-
+// controller to send file to the frontend as response
 const sendfilename = (req, res) => {
     res.json({ "fileName": req.file.filename });
 }
-
+// controller to down load file
 const downloadFile = (req, res) => {
     const fileName = req.params.filename;
     const path = __basedir + "/uploads/";
--- backend/controllers/tweet_controller.js
+++ backend/controllers/tweet_controller.js
@@ -1,7 +1,8 @@
 const Tweet = require("../models/Tweet");
-
+// create tweet controller
 const createtweet = async (req, res) => {
     try {
+        // destructuring data
         const { content, picture } = req.body;
         if (!content) {
             return res.status(400).json({
@@ -17,9 +18,9 @@ const createtweet = async (req, res) => {
         console.log(error);
     }
 }
-
+// like controller
 const liketweet = async (req, res) => {
-    try {
+    try {  // destructuring data
         const { id } = req.params;
         const tweetfound = await Tweet.findById(id)
 
@@ -50,9 +51,9 @@ const liketweet = async (req, res) => {
     }
 
 }
-
+// dislike controller
 const disliketweet = async (req, res) => {
-    try {
+    try {  // destructuring data
         const { id } = req.params;
 
         const tweetfound = await Tweet.findById(id)
@@ -83,10 +84,10 @@ const disliketweet = async (req, res) => {
     }
 
 }
-
+// reply controller
 const replytweet = async (req, res) => {
-    try {
-        const { content ,picture} = req.body;
+    try {  // destructuring data
+        const { content, picture } = req.body;
         // const picture = req.file.path;
         if (!content) {
             return res.status(400).json({
@@ -95,12 +96,11 @@ const replytweet = async (req, res) => {
         }
 
         const newTweet = new Tweet({
-            content: content,picture:picture, tweetedby: req.user,
+            content: content, picture: picture, tweetedby: req.user,
             //  replyingto: req.params.id
         });
         await newTweet.save();
-        // res.status(201).json({ message: "Tweet Added" })
-        // console.log(req.params.id);
+
         await Tweet.findByIdAndUpdate(req.params.id, {
             $push: { replies: newTweet._id }
         }, {
@@ -112,8 +112,9 @@ const replytweet = async (req, res) => {
         console.log(error);
     }
 }
-
+// detail of particular tweet using id
 const gettweetbyid = async (req, res) => {
+    // destructuring data
     const { id } = req.params;
     try {
         const tweet = await Tweet.findById(id).populate("tweetedby").populate("replies").sort({ createdAt: -1 }).populate({
@@ -129,8 +130,9 @@ const gettweetbyid = async (req, res) => {
         res.status(500).send({ message: "Internal Error Occurred" });
     }
 };
-
+// delete tweet uning id
 const deletetweet = async (req, res) => {
+    // destructuring data
     const { id } = req.params;
     const tweetfound = await Tweet.findById(id)
         .populate("tweetedby")
@@ -152,8 +154,9 @@ const deletetweet = async (req, res) => {
         res.json({ message: "not your tweet how dare you to think even about deleting this" });
     }
 }
-
+// retweet controller
 const retweet = async (req, res) => {
+    // destructuring data
     const { id } = req.params;
     try {
         const tweeted = await Tweet.findOne({ _id: id, retweetedby: req.user._id });
@@ -182,7 +185,7 @@ const retweet = async (req, res) => {
         console.log(error);
     }
 }
-
+// thiss will give all the tweets for the tweetlist page
 const getalltweet = async (req, res) => {
     try {
         const data = await Tweet.find().sort({ createdAt: -1 }).populate("tweetedby").populate("replyingto");
